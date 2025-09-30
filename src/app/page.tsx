54837322004a1f21fef46d952876be31aa09561c
kl3inIT <dathip04@gmail.com>
2025-09-30 20:50:20 +0700

    chore: update project configuration files and add Prettier and ESLint settings for improved code quality and consistency
    chore: standardize quotation marks to single quotes across multiple files and remove unused prettier configuration file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-      <main className="flex min-h-screen items-center justify-center bg-black text-green-400 font-mono">
-        <h1>Hello, Terminal Portfolio!</h1>
-      </main>
+    <main className='flex min-h-screen items-center justify-center bg-black font-mono text-green-400'>
+      <h1>Hello, Terminal Portfolio!</h1>
+    </main>
   );
 }
